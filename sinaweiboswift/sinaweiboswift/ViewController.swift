5434f0edc1d949348ad75a7dea432a6078511972
yaojianwei <yaojianwei@meicai.cn>
2016-08-10 15:55:00 +0800

    no message

---
+++ sinaweiboswift/sinaweiboswift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sinaweiboswift
+//
+//  Created by cain on 16/8/10.
+//  Copyright © 2016年 cain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
