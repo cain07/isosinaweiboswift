9ed5d98f7a890391d782304ab90b23d09e1559d9
cain007 <cain2009@163.com>
2016-08-12 08:30:14 +0800

    no message

--- sinaweiboswift/sinaweiboswift/Classes/Module/Base/VisitorLoginView/VisitorLoginView.swift
+++ sinaweiboswift/sinaweiboswift/Classes/Module/Base/VisitorLoginView/VisitorLoginView.swift
@@ -28,6 +28,32 @@ class VisitorLoginView: UIView {
     }
     
     private func setupUI(){
-        backgroundColor = UIColor.redColor()
+       // backgroundColor = UIColor.redColor()
+        addSubview(imgCircle)
+        addSubview(tipLabel)
+        //addSubview(laowang)
     }
+    
+    private lazy var imgCircle:UIImageView = UIImageView(image: UIImage(named: "visitordiscover_feed_image_smallicon"))
+    
+    //private lazy var laowang:UIImageView = UIImageView(image: UIImage(named: "visitordiscover_image_profile"))
+    
+    
+    private lazy var iconView:UIImageView = UIImageView(image: UIImage(named: "visitordiscover_feed_image_house"))
+    
+    private lazy var tipLabel:UILabel = {
+        let l = UILabel()
+        l.text = "关注一些人的 关注一些士气，你说呢的撒 打了多少放假啊上岛咖啡"
+        l.textAlignment = NSTextAlignment.Center
+        l.textColor = UIColor.orangeColor()
+        l.sizeToFit()
+        return l
+    }()
+    
+    
+    
+    
+    
+    
+    
 }
