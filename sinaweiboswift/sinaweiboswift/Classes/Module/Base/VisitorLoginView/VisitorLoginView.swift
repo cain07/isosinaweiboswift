31279a2ae1b5c16accd6e1f674560e575944ad30
yaojianwei <yaojianwei@meicai.cn>
2016-08-12 15:25:54 +0800

    no message

--- sinaweiboswift/sinaweiboswift/Classes/Module/Base/VisitorLoginView/VisitorLoginView.swift
+++ sinaweiboswift/sinaweiboswift/Classes/Module/Base/VisitorLoginView/VisitorLoginView.swift
@@ -30,11 +30,71 @@ class VisitorLoginView: UIView {
     private func setupUI(){
        // backgroundColor = UIColor.redColor()
         addSubview(imgCircle)
+        addSubview(imgBack)
+        
+        addSubview(iconView)
         addSubview(tipLabel)
+        addSubview(rigistBtn)
+        addSubview(loginBtn)
+        
+        
+        
         //addSubview(laowang)
+        
+        for v in subviews {
+            v.translatesAutoresizingMaskIntoConstraints = false
+        }
+
+        
+        addConstraint( NSLayoutConstraint(item: imgCircle, attribute: .CenterX, relatedBy: .Equal, toItem: self, attribute: .CenterX, multiplier: 1.0, constant: 0))
+        addConstraint( NSLayoutConstraint(item: imgCircle, attribute: .CenterY, relatedBy: .Equal, toItem: self, attribute: .CenterY, multiplier: 1.0, constant: 0))
+        
+        addConstraint( NSLayoutConstraint(item: iconView, attribute: .CenterX, relatedBy: .Equal, toItem: imgCircle, attribute: .CenterX, multiplier: 1.0, constant: 0))
+        addConstraint( NSLayoutConstraint(item: iconView, attribute: .CenterY, relatedBy: .Equal, toItem: imgCircle, attribute: .CenterY, multiplier: 1.0, constant: 0))
+        
+        addConstraint( NSLayoutConstraint(item: tipLabel, attribute: .CenterX, relatedBy: .Equal, toItem: iconView, attribute: .CenterX, multiplier: 1.0, constant: 0))
+        addConstraint( NSLayoutConstraint(item: tipLabel, attribute: .CenterY, relatedBy: .Equal, toItem: iconView, attribute: .Bottom, multiplier: 1.0, constant: 86))
+        
+        
+        addConstraint( NSLayoutConstraint(item: tipLabel, attribute: .Width, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1.0, constant: 323))
+        
+        
+        //左边按钮
+        addConstraint( NSLayoutConstraint(item: loginBtn, attribute: .Left, relatedBy: .Equal, toItem: tipLabel, attribute: .Left, multiplier: 1.0, constant: 0))
+        addConstraint( NSLayoutConstraint(item: loginBtn, attribute: .Top, relatedBy: .Equal, toItem: tipLabel, attribute: .CenterY, multiplier: 1.0, constant: 26))
+        
+        //按钮宽 高
+        addConstraint( NSLayoutConstraint(item: loginBtn, attribute: .Width, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1.0, constant: 150))
+        
+        addConstraint( NSLayoutConstraint(item: loginBtn, attribute: .Height, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1.0, constant: 30))
+        
+        //右边按钮
+        addConstraint( NSLayoutConstraint(item: rigistBtn, attribute: .Right, relatedBy: .Equal, toItem: tipLabel, attribute: .Right, multiplier: 1.0, constant: 0))
+        addConstraint( NSLayoutConstraint(item: rigistBtn, attribute: .Top, relatedBy: .Equal, toItem: tipLabel, attribute: .CenterY, multiplier: 1.0, constant: 26))
+        
+        //按钮宽 高
+        addConstraint( NSLayoutConstraint(item: rigistBtn, attribute: .Width, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1.0, constant: 150))
+        
+        addConstraint( NSLayoutConstraint(item: rigistBtn, attribute: .Height, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1.0, constant: 30))
+        
+        
+        
+        //遮盖层
+//        addConstraint( NSLayoutConstraint(item: imgBack, attribute: .CenterX, relatedBy: .Equal, toItem: self, attribute: .CenterX, multiplier: 1.0, constant: 0))
+//        addConstraint( NSLayoutConstraint(item: imgBack, attribute: .CenterY, relatedBy: .Equal, toItem: self, attribute: .CenterY, multiplier: 1.0, constant: 0))
+        
+        addConstraints( NSLayoutConstraint.constraintsWithVisualFormat("H:|-0-[imgBack]-0-|", options: [], metrics: nil, views: ["imgBack":imgBack]))
+        
+        addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|-0-[imgBack]-(-30)-[rigistBtn]", options: [], metrics: nil, views: ["imgBack":imgBack,"rigistBtn":rigistBtn]))
+        
+        backgroundColor = UIColor(white: 0.93, alpha: 1)
+        
     }
     
     private lazy var imgCircle:UIImageView = UIImageView(image: UIImage(named: "visitordiscover_feed_image_smallicon"))
+    
+    private lazy var imgBack:UIImageView = UIImageView(image: UIImage(named: "visitordiscover_feed_mask_smallicon"))
+
     
     //private lazy var laowang:UIImageView = UIImageView(image: UIImage(named: "visitordiscover_image_profile"))
     
@@ -47,11 +107,26 @@ class VisitorLoginView: UIView {
         l.textAlignment = NSTextAlignment.Center
         l.textColor = UIColor.orangeColor()
         l.sizeToFit()
+        l.numberOfLines = 0
         return l
     }()
     
     
+    private lazy var loginBtn:UIButton = {
+       let btn = UIButton()
+        btn.setTitle("登陆", forState: .Normal)
+        btn.setBackgroundImage(UIImage(named: "common_button_white_disable"), forState: .Normal)
+        btn.setTitleColor(UIColor.orangeColor(), forState: .Normal)
+        return btn
+    }()
     
+    private lazy var rigistBtn:UIButton = {
+        let btn = UIButton()
+        btn.setTitle("注册", forState: .Normal)
+        btn.setBackgroundImage(UIImage(named: "common_button_white_disable"), forState: .Normal)
+        btn.setTitleColor(UIColor.orangeColor(), forState: .Normal)
+        return btn
+    }()
     
     
     
